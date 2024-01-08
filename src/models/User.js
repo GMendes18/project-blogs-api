9898bdf5bee56d519c31fb0d38ce3ba18105f41c
Gabriel <gabri.mevial@gmail.com>
2024-01-08 19:52:18 -0300

    requisito 2 completo

---
+++ src/models/User.js
@@ -0,0 +1,30 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    displayName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+    },
+  }, {
+    tableName: 'Users', 
+    underscored: true, 
+    timestamps: false,
+  });
+  return User;
+};
