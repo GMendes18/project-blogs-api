205de4a3e4a006d3b592d862770385492b23c086
Gabriel <gabri.mevial@gmail.com>
2024-01-09 22:09:52 -0300

    requisito 10

--- src/middlewares/tokenValidation.js
+++ src/middlewares/tokenValidation.js
@@ -8,7 +8,8 @@ const authenticateToken = async (req, res, next) => {
   }
   const token = bearerToken.split(' ')[1];
   try {
-    req.user = jwt.verify(token, process.env.JWT_SECRET);
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decodedToken;
     next();
   } catch (error) {
     return res.status(401).json({ message: 'Expired or invalid token' });
---
+++ src/models/BlogPost.js
@@ -0,0 +1,44 @@
+module.exports = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define('BlogPost', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'User',
+        key: 'id',
+      },
+    },
+    published: {
+      type: DataTypes.DATE,
+    },
+    updated: {
+      type: DataTypes.DATE,
+    },
+  }, {
+    tableName: 'blog_posts',
+    underscored: true,
+  });
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User, {
+      foreignKey: 'userId',
+      as: 'user',
+    });
+  };
+
+  return BlogPost;
+};
