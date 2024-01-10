c98cd76dd030b3d87fc7388f84446209330d53ca
Gabriel <gabri.mevial@gmail.com>
2024-01-09 22:25:31 -0300

    requisito 11 finalizado

---
+++ src/models/PostCategory.js
@@ -0,0 +1,33 @@
+module.exports = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define('PostCategory', {
+    postId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+    },
+    categoryId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+    },
+  }, {
+    tableName: 'posts_categories',
+    timestamps: false,
+    underscored: true,
+  });
+    
+  PostCategory.associate = (models) => {
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'blogpost',
+      through: PostCategory,
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+    }); 
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'categories',
+      through: PostCategory,
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+    }); 
+  };
+    
+  return PostCategory;
+};
